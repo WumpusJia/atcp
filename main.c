5c368c9f4a9213754c94e63aa20a6eeb5bf18c1d
Wumpus <wumpusjia@gmail.com>
2016-11-16 18:57:45 +0800

    First commit

---
+++ main.c
@@ -0,0 +1,91 @@
+#include<sys/socket.h>
+#include<linux/if.h>
+#include<linux/if_tun.h>
+#include<sys/ioctl.h>
+#include<fcntl.h>
+#include<stdio.h>
+#include<stdlib.h>
+#include<string.h>
+#include<stdarg.h>
+#include<errno.h>
+
+
+int tun_alloc(char* dev)
+{
+    struct ifreq ifr;
+    int fd,err;
+
+    if( (fd = open("/dev/net/tun",O_RDWR)) < 0)
+    {
+        printf("ERROR: Could not open TUN/TAP dev");
+        exit(1);
+    }
+
+
+    memset(&ifr,0,sizeof(ifr));
+
+    /* Flags: IFF_TUN   - TUN device (no Ethernet headers)
+         *        IFF_TAP   - TAP device
+         *
+         *        IFF_NO_PI - Do not provide packet information
+         */
+    ifr.ifr_flags = IFF_TUN;
+    if( *dev )
+    {
+        strncpy(ifr.ifr_name, dev, IFNAMSIZ);
+    }
+    if( (err = ioctl(fd, TUNSETIFF, (void *) &ifr)) < 0 )
+    {
+        printf("ERROR: Could not ioctl %s",strerror(errno));
+        close(fd);
+        return err;
+    }
+    strcpy(dev, ifr.ifr_name);
+    return fd;
+}
+
+#define CMD_MAX_LEN 256
+
+int cmd_run(char* format, ...)
+{
+    char buf[CMD_MAX_LEN];
+    va_list ap;
+    va_start(ap,format);
+    vsnprintf(buf,CMD_MAX_LEN,format,ap);
+    va_end(ap);
+
+    printf("RUN: %s\n",buf);
+    return system(buf);
+}
+
+
+/*
+What u need do first:
+sudo ip tuntap add mode tun dev tun0
+sudo ip addr add 10.0.0.0/24 dev tun0  # give it an ip
+sudo ip link set dev tun0 up  # bring the if up
+sudo ip route get 10.0.0.2  # check that packets to 10.0.0.x are going through tun0
+ping 10.0.0.2  # leave this running in another shell to be able to see the effect of the next example
+
+
+*/
+
+
+int main()
+{
+    int tun_fd;
+    char dev[] = "tun0";
+    tun_fd = tun_alloc(dev);
+
+
+
+    char buf[1600];
+
+    while(1)
+    {
+        int len = read(tun_fd,buf,sizeof(buf));
+        printf("%d\n",len);
+    }
+
+}
+
